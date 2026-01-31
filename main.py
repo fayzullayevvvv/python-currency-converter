086f6db92a544bd5eded7c689ff656afbe1db747
djumanov <djumanovdev@gmail.com>
2026-01-31 19:31:52 +0500

    currency converter

---
+++ main.py
@@ -0,0 +1,22 @@
+import json
+import requests
+
+
+def converter(amount: float, currency: str) -> float:
+    response = requests.get(f'https://cbu.uz/uz/arkhiv-kursov-valyut/json/{currency.upper()}/')
+
+    rate = float(json.loads(response.text)[0]['Rate'])
+
+    result = amount * rate
+    return result
+
+
+def main() -> None:
+    amount = float(input("Amount: "))
+    currency = input("Currency: ")
+
+    result = converter(amount, currency)
+
+    print(result)
+
+main()
